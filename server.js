43b3784f326d224ed5e0edf7eef0ed818988eb34
Rich McLaughlin <rsmclaug@gmail.com>
2015-06-29 14:40:44 -0400

    Added deps and upgrading to express 4.x

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ var express = require('express');
 
 var app = express();
 
-app.configure(function () {
-    app.use(express.static(__dirname));
-    app.use(express.bodyParser());
-});
+app.use(express.static(__dirname));
 
 app.listen(port);
 console.log('Listening on port ' + port);
