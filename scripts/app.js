7092abac79a06920bf7e70a206eaaf93f3613ccc
Rich McLaughlin <rsmclaug@gmail.com>
2015-06-29 15:52:07 -0400

    ng hello world

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+var skillup = angular.module('skill-up-ng-app', []);
+
+skillup.controller("MainCtrl", ["$scope", function ($scope) {
+    "use strict";
+
+    var main = this;
+    main.text = "Hello World!";
+
+}]);
--- server.js
+++ server.js
@@ -5,5 +5,9 @@ var app = express();
 
 app.use(express.static(__dirname));
 
+app.get('/hello', function(req, res){
+    res.send('hello world!');
+});
+
 app.listen(port);
 console.log('Listening on port ' + port);
